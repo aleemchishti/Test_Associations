299570e8f8259c7de2b35cbcbd8f51c1e7c30814
aleemchishti <chishti_aleem@yahoo.com>
2022-11-24 09:08:24 -0500

    validations for users and articles model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 	has_many :articles
+	validates :username, uniqueness: {case_senstive: false}, length: {minimum:6, maximum:14}
+	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: {case_senstive: false}, length: {maximum:100}, format: {with: VALID_EMAIL_REGEX }
+
 end
